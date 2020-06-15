25360b31189a37e63397abc424df74820cfe4a9a
傅成涵 <327559322@qq.com>
2020-06-15 18:04:35 +0800

    新增

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
+		<!--<image v-if="imgUrl.length > 0" class="logo" :src="imgUrl"></image>-->
+		<button open-type='getUserInfo'  @getuserinfo="getUserInfo">生成头像</button>
+		<canvas :style="{ width: cav_width+'rpx', height: cav_height + 'rpx' }"
+		canvas-id="merge-img"></canvas>
 	</view>
 </template>
 
@@ -11,32 +11,78 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				imgUrl:'',
+				cav_width:720,
+				cav_height:400
 			}
 		},
 		onLoad() {
-
+			console.log(1)
+			console.log(uni.getSystemInfoSync().windowWidth)
+			console.log(2)
 		},
 		methods: {
+			getUserInfo(){
+							uni.getUserInfo({
+								provider: uni.getProvider,
+								success: (infoRes) => {
+									console.log(infoRes)
+									console.log('用户昵称为：' + infoRes.userInfo.nickName);
+									this.nickName = infoRes.userInfo.nickName
+									this.imgUrl = infoRes.userInfo.avatarUrl.replace('/132','/0')
 
+									
+									uni.getImageInfo({
+										src:infoRes.userInfo.avatarUrl.replace('/132','/0'),
+										success:(res)=>{
+											let width = uni.getSystemInfoSync().windowWidth
+											this.cav_width = 750
+											this.cav_height = 750
+											const ctx = uni.createCanvasContext('merge-img',this)
+											console.log(res)
+											ctx.drawImage(res.path,0,0,width,width)	
+											ctx.draw(true,()=>{
+												ctx.drawImage('../../static/backImg.jpg',0,0,width,width)
+												ctx.draw(true,()=>{
+													setTimeout(()=>{
+														uni.canvasToTempFilePath({
+														canvasId:'merge-img',
+														/*destWidth:this.cav_width/2,
+														destHeight:this.cav_height/2,*/
+														width:width,
+														height:width,
+														success:(res)=>{
+															console.log(res)
+															uni.saveImageToPhotosAlbum({
+																filePath:  res.tempFilePath,
+																success:(res)=>{
+																	console.log(res)
+																},
+																fail:(res)=>{
+																	console.log(res)
+																}
+															})
+														},
+														fail:(res)=>{
+															console.log(res)
+														}
+												})
+													},500)
+													
+												})
+											})
+										}
+									})
+								},
+								fail: function (infoRes) {
+									console.log(infoRes)
+								}
+							});
+						},
 		}
 	}
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
 </style>
